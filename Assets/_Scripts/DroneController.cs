3807cde49d6f733bed05e2ff2d8328fbe126d49c
Aazriik <dibartemis@gmail.com>
2026-06-14 17:06:26 -0400

    Turning and Ground Check

--- Assets/_Scripts/DroneController.cs
+++ Assets/_Scripts/DroneController.cs
@@ -32,20 +32,30 @@ public class DroneController : MonoBehaviour
     // Effective Height determines how far the Drone will fly in the UP direction.
     public float effectiveHeight;
     // Engine Lift is Throttle Power.
-    public float EngineLift = 0.0075f;
+    public float engineLift = 0.0075f;
     // Forward Movement Speed.
-    public float ForwardForce;
+    public float forwardForce;
     // Backward Movement Speed.
-    public float BackwardForce;
+    public float backwardForce;
+
+    // Turning.
+    public float turnForce;
+    private float turnForceHelper;
+    private float turning = 0f;
 
     // Vector 2 to Handle Movement.
     private Vector2 movement = Vector2.zero;
+
+    // Ground Check.
+    public LayerMask groundLayer;
+    private float distanceToGround;
+    public bool isGrounded = true;
 
     // Component References.
     private Rigidbody rb;
 
     // Pause Menu References.
-    public GameObject PauseDisplay;
+    public GameObject pauseDisplay;
     bool isPaused = false;
 
     #endregion
@@ -69,13 +79,14 @@ public class DroneController : MonoBehaviour
     {
         // Get Component References.
         rb = GetComponent<Rigidbody>();
-        bool isPaused = PauseDisplay.activeInHierarchy;
+        bool isPaused = pauseDisplay.activeInHierarchy;
 
     }
 
     // Update is called once per frame.
     void Update()
     {
+        HandleGroundCheck();
         HandleInputs();
     }
 
@@ -89,54 +100,92 @@ public class DroneController : MonoBehaviour
     // Function to handle all inputs in the Update Method.
     void HandleInputs()
     {
-        // Movement (Horizontal and Vertical).
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
-        // Move Forward.
-        if (InputActions.FindAction("AM_Drone/MoveForward").IsPressed())
+        if (!isGrounded)
         {
-            
-        }
+            //Debug.Log("Is NOT Grounded");
 
-        // Move Backward.
-        if (InputActions.FindAction("AM_Drone/MoveBackward").IsPressed())
-        {
+            // Movement (Horizontal and Vertical).
+            movement.x = Input.GetAxis("Horizontal");
+            movement.y = Input.GetAxis("Vertical");
 
-        }
+            #region Movement Input Actions
+            // Move Forward.
+            if (InputActions.FindAction("AM_Drone/MoveForward").IsPressed())
+            {
 
-        // Move Left.
-        if (InputActions.FindAction("AM_Drone/MoveLeft").IsPressed())
-        {
-            
-        }
+            }
 
-        // Move Right.
-        if (InputActions.FindAction("AM_Drone/MoveRight").IsPressed())
-        {
+            // Move Backward.
+            if (InputActions.FindAction("AM_Drone/MoveBackward").IsPressed())
+            {
 
-        }
+            }
 
-        // Descend.
-        if (InputActions.FindAction("AM_Drone/Descend").IsPressed())
-        {
-            EnginePower -= EngineLift;
+            // Move Left.
+            if (InputActions.FindAction("AM_Drone/MoveLeft").IsPressed())
+            {
 
-            if (EnginePower < 0) EnginePower = 0;
+            }
+
+            // Move Right.
+            if (InputActions.FindAction("AM_Drone/MoveRight").IsPressed())
+            {
+
+            }
+
+            #endregion
         }
 
         // Ascend.
         if (InputActions.FindAction("AM_Drone/Ascend").IsPressed())
         {
-            EnginePower += EngineLift;
+            EnginePower += engineLift;
         }
+
+        // Descend.
+        if (InputActions.FindAction("AM_Drone/Descend").IsPressed())
+        {
+            EnginePower -= engineLift;
+
+            if (EnginePower < 0) EnginePower = 0;
+        }
+        //Debug.Log("Is Grounded");
+
+    }
+
+    void HandleGroundCheck()
+    {
+        // Create Raycast shooting downward.
+        RaycastHit hit;
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        Ray ray = new Ray(transform.position, direction);
+
+        // Check if the Raycast hits the Ground Layer Mask within 3000 units.
+        if (Physics.Raycast(ray, out hit, 3000, groundLayer))
+        {
+            distanceToGround = hit.distance;
+
+            // Check if Ground is less than 2 units away and set IsGrounded to True.
+            if (distanceToGround < 0.2f) isGrounded = true;
+            else isGrounded = false;
+        }
+
     }
 
     // Ascend with Upward Force using Rigidbody while factoring in mass.
     void DroneHover()
     {
-        float upForce = 1 - Mathf.Clamp(rb.transform.position.y / effectiveHeight, 0, 1);
-        upForce = Mathf.Lerp(0, EnginePower, upForce) * rb.mass;
-        rb.AddRelativeForce(Vector3.up * upForce);
+        if (InputActions.FindAction("AM_Drone/Ascend").IsPressed())
+        {
+            //EnginePower += engineLift;
+
+            float upForce = 1 - Mathf.Clamp(rb.transform.position.y / effectiveHeight, 0, 1);
+            upForce = Mathf.Lerp(0, EnginePower, upForce) * rb.mass;
+            rb.AddRelativeForce(Vector3.up * upForce);
+        }
+        //float upForce = 1 - Mathf.Clamp(rb.transform.position.y / effectiveHeight, 0, 1);
+        //upForce = Mathf.Lerp(0, EnginePower, upForce) * rb.mass;
+        //rb.AddRelativeForce(Vector3.up * upForce);
         
     }
 
@@ -145,12 +194,28 @@ public class DroneController : MonoBehaviour
         // Move Forward Z-Axis.
         if (InputActions.FindAction("AM_Drone/MoveForward").IsPressed())
         {
-            rb.AddRelativeForce(Vector3.forward * Mathf.Max(0f, movement.y * ForwardForce * rb.mass));
+            rb.AddRelativeForce(Vector3.forward * Mathf.Max(0f, movement.y * forwardForce * rb.mass));
         }
         // Move Backward Z-Axis.
         else if (InputActions.FindAction("AM_Drone/MoveBackward").IsPressed())
         {
-            rb.AddRelativeForce(Vector3.back * Mathf.Max(0f, movement.y * BackwardForce * rb.mass));
+            rb.AddRelativeForce(Vector3.back * Mathf.Max(0f, -movement.y * backwardForce * rb.mass));
+        }
+        // Rotate Right.
+        if (InputActions.FindAction("AM_Drone/MoveRight").IsPressed())
+        {
+            // Drone Turns RIGHT.
+            float turn = turnForce * Mathf.Lerp(movement.x, movement.x * (turnForceHelper - Mathf.Abs(movement.y)), Mathf.Max(0f, movement.y));
+            turning = Mathf.Lerp(turning, turn, Time.fixedDeltaTime * turnForce);
+            rb.AddRelativeTorque(1f, movement.x * rb.mass, 0f);
+        }
+        // Rotate Left.
+        if (InputActions.FindAction("AM_Drone/MoveLeft").IsPressed())
+        {
+            // Drone Turns LEFT.
+            float turn = turnForce * Mathf.Lerp(movement.x, movement.x * (turnForceHelper - Mathf.Abs(movement.y)), Mathf.Max(0f, movement.y));
+            turning = Mathf.Lerp(turning, turn, Time.fixedDeltaTime * turnForce);
+            rb.AddRelativeTorque(0f, movement.x * rb.mass, 0f);
         }
     }
 
@@ -161,7 +226,7 @@ public class DroneController : MonoBehaviour
 
     public void OnDescend(InputAction.CallbackContext context)
     {
-        movement.y -= context.ReadValue<float>() * Time.deltaTime;
+        movement.y += context.ReadValue<float>() * Time.deltaTime;
     }
 
     public void OnMoveForward(InputAction.CallbackContext context)
@@ -171,19 +236,18 @@ public class DroneController : MonoBehaviour
 
     public void OnMoveBackward(InputAction.CallbackContext context)
     {
-
-    }
-
-    public void OnMoveLeft(InputAction.CallbackContext context)
-    {
-
+        
     }
 
     public void OnMoveRight(InputAction.CallbackContext context)
     {
-
+        movement.x += context.ReadValue<float>() * Time.fixedDeltaTime;
     }
 
+    public void OnMoveLeft(InputAction.CallbackContext context)
+    {
+        movement.x += context.ReadValue<float>() * Time.fixedDeltaTime;
+    }
 
     #region First Draft
 
@@ -253,27 +317,27 @@ public class DroneController : MonoBehaviour
         if (!isPaused)
         {
             // Toggle the Pause Menu ON.
-            PauseDisplay.SetActive(true);
+            pauseDisplay.SetActive(true);
             // Pause the game by setting time scale to 0.
             Time.timeScale = 0f;
             // If we are now paused, disable DRONE input and enable UI input.
             InputActions.FindActionMap("AM_Drone").Disable();
             InputActions.FindActionMap("AM_UI").Enable();
             // Set isPaused to the PauseDisplay active state.
-            isPaused = PauseDisplay.activeInHierarchy;
+            isPaused = pauseDisplay.activeInHierarchy;
             Debug.Log("Is Paused? " + isPaused);
         }
         else
         {
             // Toggle the Pause Menu OFF.
-            PauseDisplay.SetActive(false);
+            pauseDisplay.SetActive(false);
             // Unpause the game by setting time scale back to 1.
             Time.timeScale = 1f;
             // If we are now unpaused, disable UI input and enable DRONE input.
             InputActions.FindActionMap("AM_UI").Disable();
             InputActions.FindActionMap("AM_Drone").Enable();
             // Set isPaused to the PauseDisplay active state.
-            isPaused = PauseDisplay.activeInHierarchy;
+            isPaused = pauseDisplay.activeInHierarchy;
             Debug.Log("Is Paused? " + isPaused);
         }
     }
